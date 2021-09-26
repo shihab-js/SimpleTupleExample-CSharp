894d4c3ced0ff8c8bc09453a87a1a88c927b874e
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-26 22:52:11 +0600

    Tuple has been created...

--- SimpleTupleExample/Program.cs
+++ SimpleTupleExample/Program.cs
@@ -6,7 +6,9 @@ namespace SimpleTupleExample
     {
         static void Main(string[] args)
         {
-            
+            //create a tuple
+            var person = Tuple.Create("One",2,3,4,5,6,7,8);
+
         }
     }
 }
